a5bdbdfa4335e8b9d538bc932940dcebf271a78f
halo <beatvi96@gmail.com>
2021-09-09 18:26:42 -0300

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -10,27 +10,27 @@ function App() {
       <NavBar />
       <div className="container">
         <h1 className="text-primary py-3">Dashboard de Vendas</h1>
-        
+
         <div className="row px-3">
           <div className="col-sm-6">
-            <h5 className="text-center text-secondary">Taxa de Sucesso (%)</h5>
+            <h5 className="text-center text-secondary">Taxa de sucesso (%)</h5>
             <BarChart />
-        </div>
-        <div className="col-sm-6">
-            <h5 className="text-center text-secondary">Todas as Vendas</h5>
+          </div>
+          <div className="col-sm-6">
+            <h5 className="text-center text-secondary">Todas as vendas</h5>
             <DonutChart />
+          </div>
         </div>
-      </div>
 
-      <div className="py-3">
-        <h2 className="text-primary"></h2>
-      </div>
+        <div className="py-3">
+          <h2 className="text-primary">Todas as vendas</h2>
+        </div>
 
         <DataTable />
       </div>
-    <Footer />
+      <Footer />
     </>
   );
 }
 
-      export default App;
+export default App;
